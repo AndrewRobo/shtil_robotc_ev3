1e98e315de1753c1327a0692a9133343f5db4062
AdreyP <andrewprokopchyk34@gmail.com>
2023-12-12 23:40:17 +0300

    один и два элемента на рекурсию не посылыем

--- zima_3_betta.c
+++ zima_3_betta.c
@@ -48,7 +48,19 @@ void sort(int k, int r)
 {// rekursivnaya chast sortirovki vizivayuschayasama sebya
  // po radelennim chastyam
 
-    if (  r - k <= 1 )     {  return; }
+    if (  r - k == 0 )     {  return; }
+	if (  (r-k) == 1 )  {
+
+	 if ( nose_sorted[k] < nose_sorted[r] )
+      {
+            //swap(a[i], a[m])
+            int itmp = nose_sorted[k];
+            nose_sorted[k] = nose_sorted[r];
+            nose_sorted[r] = itmp;
+            m++;
+      }// end if if ( nose_sorted[i] < pivot )
+      return;  
+	}
 
 
     int m = partition(k, r);
