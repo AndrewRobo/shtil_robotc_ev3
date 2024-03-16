c6a28caf5bccfec40c980d929333bf1fdc0c364d
AdreyP <andrewprokopchyk34@gmail.com>
2024-03-16 14:03:22 +0300

    исправил ошибку в функции moveKyrsNoStop

--- monoptok_zadanie_1_2.c
+++ monoptok_zadanie_1_2.c
@@ -83,8 +83,8 @@ void moveKyrs(int giroTagetXZ, int stop)
 
 	void moveKyrsNoStop(int giroTagetXZ)
 	{
-		int GiroscopTargetFrozen = -280
-		int GiroscopTargetDinamik = -280
+		int GiroscopTargetFrozen = giroTagetXZ
+		int GiroscopTargetDinamik = giroTagetXZ
 
 		    while(1)
 		{
