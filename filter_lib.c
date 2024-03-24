4c94654a9414288d5842b04d3407420f720e15e6
AndyDDT <usp@mail.ru>
2024-03-24 15:27:21 +0300

    счетчики фильтров

--- filter_lib.c
+++ filter_lib.c
@@ -9,6 +9,7 @@ int filtr_itog_right;
     const int len_left_real = 17;
     int arr_left[len_left_real];
     int ih_left = 0;
+    int last_count_left=0;
 
     const int len_left_frozen = 11;
     const int middle_left = len_left_frozen/2;
@@ -18,6 +19,7 @@ int filtr_itog_right;
     const int len_right_real = 19;
     int arr_right[len_right_real];
     int ih_right = 0;
+    int last_count_right=0;
 
     const int len_right_frozen = 11;
     const int middle_right = len_right_frozen/2;
@@ -27,6 +29,7 @@ int filtr_itog_right;
     const int len_nose_real = 300;
     int arr_nose[len_nose_real];
     int ih_nose = 0;
+    int last_count_nose-0;
 
     const int len_nose_frozen = 24;
     const int middle_nose = len_nose_frozen/2;
@@ -72,17 +75,17 @@ task sensors()
     while (1)
     {
         arr_left[ ih_left ] = SensorValue(port_left);
-        ih_left++;
+        ih_left++;last_count_left++;
         if (ih_left >= len_left_real)	{ih_left = 0;}
         sleep(10);
 
         arr_right[ih_right] = SensorValue(port_right);
-        ih_right++;
+        ih_right++;last_count_right++;
         if (ih_right >= len_right_real)		{ih_right = 0;}
         sleep(10);
 
         arr_nose[ih_nose] = SensorValue(port_nose);
-        ih_nose++;
+        ih_nose++;last_count_nose++;
         if (ih_nose >= len_nose_real)	{ih_nose = 0;}
         sleep(10);
     } // while(1)
@@ -90,19 +93,25 @@ task sensors()
 
 task filtr()
 {
-    time1[0]=0;
+    //time1[0]=0;
     while(1)
     {
-        my_timer=time1[0];
+        //my_timer=time1[0];
+        if ( last_count_right >= len_right_frozen){
+             last_count_right =0;
+            frozen( arr_right, len_right_real, ih_right, arr_right_frozen, len_right_frozen);
+            buble_sort( arr_right, len_right_real );
+            filtr_itog_right = arr_right_frozen[middle_right];
+        } else {sleep(100)}
 
-        frozen( arr_right, len_right_real, ih_right, arr_right_frozen, len_right_frozen);
-        buble_sort( arr_right, len_right_real );
-        filtr_itog_right = arr_right_frozen[middle_right];
-        frozen( arr_nose, len_nose_real, ih_nose, arr_nose_frozen, len_nose_frozen);
-        buble_sort( arr_nose, len_nose_real );
-        filtr_itog_nose = arr_nose_frozen[middle_nose];
-
-        my_delta_timer=time1[0]- my_timer ;
+        if( last_count_nose >= len_nose_frozen) {
+            last_count_nose=0;
+        
+            frozen( arr_nose, len_nose_real, ih_nose, arr_nose_frozen, len_nose_frozen);
+            buble_sort( arr_nose, len_nose_real );
+            filtr_itog_nose = arr_nose_frozen[middle_nose];
+        }else{sleep(100)}
+        //my_delta_timer=time1[0]- my_timer ;
         //displayBigTextLine(10, "%d", my_delta_timer);
     }// while(1)
 }//task filtr()
