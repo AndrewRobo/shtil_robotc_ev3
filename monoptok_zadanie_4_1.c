45f7a85b1d1aab077337cb29947196b4b2174e71
AdreyP <andrewprokopchyk34@gmail.com>
2024-03-28 16:35:04 +0300

    оптимизировал нужные функции выкинул лишнюю под программу

--- monoptok_zadanie_4_1.c
+++ monoptok_zadanie_4_1.c
@@ -10,7 +10,7 @@
 #include "init_lib.c"
 //global variable
 
-const int distans_ot_robota_do_borta=20;
+const int distans_ot_robota_do_borta=30;
 const int v_max=100;
 
 void moveProporcional(int GiroscopTarget, int koef_usilenia , int v_max)
@@ -21,79 +21,50 @@ void moveProporcional(int GiroscopTarget, int koef_usilenia , int v_max)
 	motor[mot_right]=v_max-Error_ygol*koef_usilenia;
 }//void moveProporcional(int GiroscopTarget, int koef_usilenia , int v_max)
 
-void povorot(int ugol_povorota, int v_max)
-{
-	setLEDColor(ledGreen);
-	if(ugol_povorota<0)
-	{
-		while( ugol_povorota+5 > SensorValue(port_gyro))
-		{
-			int Error_ygol = ugol_povorota - SensorValue(port_gyro);
-			motor[mot_left]=Error_ygol;
-			motor[mot_right]=-Error_ygol;
-		}//while( ugol_povorota+5 != SensorValue(port_gyro))
-	}//if(ugol_povorota<0)
-	else
-	{
-		while( ugol_povorota-5 < SensorValue(port_gyro))
-		{
-			int Error_ygol = ugol_povorota - SensorValue(port_gyro);
-			motor[mot_left]=Error_ygol;
-			motor[mot_right]=-Error_ygol;
-		}//while( ugol_povorota-5 != SensorValue(port_gyro))
-	}//else	if(ugol_povorota<0)
-}//void povorot(int ugol_povorota, int v_max)
-
 void moveKyrsRight(int giroTagetXZ, int stoop)
 {
 	setLEDColor(ledOrange);
+
 	int GiroscopTargetFrozen = giroTagetXZ;
 	int GiroscopTargetDinamik = giroTagetXZ;
-	while(1)
-	{
-		if(stoop<SensorValue(port_nose))
+	int delta_distans_right
+
+	do{
+		delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_right);
+		GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
+		if(abs(delta_distans_right)<10)
+		{	moveProporcional( GiroscopTargetDinamik, 1 , v_max);	}
+		else
 		{
-			int delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_right);
-			GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
-			if(abs(delta_distans_right)<10)
-			{	moveProporcional( GiroscopTargetDinamik, 1 , v_max);	}
+			if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
+			{	moveProporcional( GiroscopTargetFrozen-15, 1 , v_max);	}
 			else
-			{
-				if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
-				{	moveProporcional( GiroscopTargetFrozen-15, 1 , v_max);	}
-				else
-				{	moveProporcional( GiroscopTargetFrozen+15, 1 , v_max);	}
-			}//if(abs(delta_distans_right)<10)
-		}//if(70<filtr_itog_nose)
-		else//if(70>filtr_itog_nose)
-		{ break; }
-	}// while(1)
+			{	moveProporcional( GiroscopTargetFrozen+15, 1 , v_max);	}
+		}//if(abs(delta_distans_right)<10)
+	}while(stoop<SensorValue(port_nose))
 }//void moveKyrsRight(int giroTagetXZ, int stoop)
 
 void moveKyrsLeft(int giroTagetXZ, int stoop)
 {
 	setLEDColor(ledOrange);
+	
 	int GiroscopTargetFrozen = giroTagetXZ;
 	int GiroscopTargetDinamik = giroTagetXZ;
-	while(1)
-	{
-		if(stoop<SensorValue(port_nose))
+	int delta_distans_left
+
+	do{
+		delta_distans_left =  distans_ot_robota_do_borta - SensorValue(port_left);
+		GiroscopTargetDinamik = GiroscopTargetFrozen + delta_distans_left;
+		if(abs(delta_distans_left)<10)
+		{	moveProporcional( GiroscopTargetDinamik, 1 , v_max);	}
+		else
 		{
-			int delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_right);
-			GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
-			if(abs(delta_distans_right)<10)
-			{	moveProporcional( GiroscopTargetDinamik, 1 , v_max);	}
+			if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
+			{	moveProporcional( GiroscopTargetFrozen+15, 1 , v_max);	}
 			else
-			{
-				if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
-				{	moveProporcional( GiroscopTargetFrozen-15, 1 , v_max);	}
-				else
-				{	moveProporcional( GiroscopTargetFrozen+15, 1 , v_max);	}
-			}//if(abs(delta_distans_right)<10)
-		}//if(70<filtr_itog_nose)
-		else//if(70>filtr_itog_nose)
-		{ break; }
-	}// while(1)
+			{	moveProporcional( GiroscopTargetFrozen-15, 1 , v_max);	}
+		}//if(abs(delta_distans_left)<10)
+	}while(stoop<SensorValue(port_nose))
 }//void moveKyrsLeft(int giroTagetXZ, int stoop)
 
 void povorot_na_1_motore(int prohlii_ygol ,int ugol_povorota, int v_max)
@@ -214,11 +185,16 @@ task main()
 
 	povorot_na_1_motore(-180, -210 , 100 );
 
-	EnMoveGir( 1500 , -210 );
+	EnMoveGir( 1700 , -210 );
 
 	povorot_na_1_motore(-210, -180 , 100 );
 
-	EnMoveGirLeft( 2500 , -180 );
+	EnMoveGirLeft( 2000 , -180 );
+
+	moveKyrsLeft(-180 , 75);
+
+	povorot_na_1_motore(-180 , -90 , 100);
+
 
 /////
 
