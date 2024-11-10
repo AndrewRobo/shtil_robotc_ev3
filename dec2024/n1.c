23ba0570de6c3e663ca12bb86515d4c551556f62
AdreyP <andrewprokopchyk34@gmail.com>
2024-11-10 20:23:01 +0300

    снес ВСЕ лишние из n1

--- dec2024/n1.c
+++ dec2024/n1.c
@@ -18,61 +18,6 @@ const int v_max=1000;
 //include
 #include "lib/init_lib.c"
 #include "lib/move_lib.c"
-//voids
-/*
-
-void moveProporcional( int GiroscopTarget, int koef_usilenia  , int v_max )
-
-{
-	const int k_gyro_rul = 1;
-	int dynamic_speed = v_max;
-
-	int ugol_error = GiroscopTarget -  SensorValue(port_gyro);
-	dynamic_speed -= koef_usilenia * ( abs( ugol_error ) );
-	if ( dynamic_speed > v_max ) 	    dynamic_speed=v_max;
-	else if (dynamic_speed < -v_max )	dynamic_speed=-v_max;
-	else if ( dynamic_speed>0 && dynamic_speed<=15 )  dynamic_speed=0;
-	else if ( dynamic_speed<0 && dynamic_speed>=-15 ) dynamic_speed=0;
-	if ( ugol_error <=0 ){
-		motor[mot_left]=dynamic_speed;
-		motor[mot_right]=v_max;
-	}
-	else {
-		motor[mot_left]=v_max;
-		motor[mot_right]=dynamic_speed;
-	}
-
-	set_ugol_rul ( k_gyro_rul * ugol_error);
-
-}
-*/
-
-
-void moveKyrs(int giroTagetXZ, int stoop)
-{
-	setLEDColor(ledOrange);
-	int GiroscopTargetFrozen = giroTagetXZ;
-	int GiroscopTargetDinamik = giroTagetXZ;
-	while(1)
-	{
-		if(stoop<SensorValue(port_nose))
-		{
-			int delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_right);
-			GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
-			if(abs(delta_distans_right)<10)
-			{	correct_kurs( GiroscopTargetDinamik, 1 , v_max);	}
-			else
-			{
-				if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
-				{	correct_kurs( GiroscopTargetFrozen-15, 1 , v_max);	}
-				else
-				{	correct_kurs( GiroscopTargetFrozen+15, 1 , v_max);	}
-			}//if(abs(delta_distans_right)<10)
-		}//if(70<filtr_itog_nose)
-		else//if(70>filtr_itog_nose)
-		{ break; }
-	}// while(1)
-}//void moveKyrs()
 
 task main()
 {
