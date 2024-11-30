3f8428b3027ef36d140866461d4fee6ed6f98f03
AndyDDT <usp@mail.ru>
2024-11-30 17:18:14 +0300

    25 sek pause sounds

---
+++ dec2024/lib/lib_globals.c
@@ -0,0 +1,9 @@
+int  distans_right_bort=30;
+int  distans_left_bort=30;
+
+int distans_ot_robota_do_borta=30; // dlya sovmestimosti
+
+int  distans_right_bui=30;
+int  distans_left_bui=30;
+
+int  vmax=100
--- dec2024/lib/lib_mvGyroleftToRightBui.c
+++ dec2024/lib/lib_mvGyroleftToRightBui.c
@@ -12,7 +12,7 @@ void mvGyroLeftToRightBui(int giroTagetXZ, int LeftBui)
 
 	while(LeftBui<SensorValue(port_right))
 	{
-			sleep(15);
+			sleep(25);
 			int delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_left);
 			GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
 			if(abs(delta_distans_right)<10)
