2833868e2f6fb5a5793df4c2ca2e8fed324af3a2
AndyDDT <usp@mail.ru>
2024-12-05 18:19:19 +0300

    turn_lib поправил

--- dec2024/lib/lib_turn.c
+++ dec2024/lib/lib_turn.c
@@ -6,29 +6,30 @@ void turn(int new_kurs, int v_max)
 
 	setLEDColor(ledGreen);
 
-	int ugol_povorota = new_kurs;
+	
 
-	int delta_ugol = ugol_povorota - SensorValue(port_gyro);
-
+	int delta_ugol = new_kurs - SensorValue(port_gyro);
+	int Error_ygol = delta_ugol;
 
 	if( delta_ugol < 0 )
 	{
-		while( ugol_povorota+5 < SensorValue(port_gyro))
-		{
-			int Error_ygol = ugol_povorota - SensorValue(port_gyro);
+		while( Error_ygol < (-5) )
+		{	
+			Error_ygol = new_kurs - SensorValue(port_gyro);
 			motor[mot_left]=Error_ygol;
 			motor[mot_right]=v_max;
-			set_ugol_rul(-40);
+			set_ugol_rul(Error_ygol)
 		}//while( ugol_povorota+5 != SensorValue(port_gyro))
+
 	}//if(ugol_povorota<0)
 	else
 	{
-		while( ugol_povorota-5 > SensorValue(port_gyro))
+		while( Error_ygol > 5 )
 		{
-			int Error_ygol = ugol_povorota - SensorValue(port_gyro);
+			Error_ygol = new_kurs - SensorValue(port_gyro);
 			motor[mot_left]=v_max;
 			motor[mot_right]=Error_ygol;
-			set_ugol_rul(40);
+			set_ugol_rul(Error_ygol);
 		}//while( ugol_povorota-5 != SensorValue(port_gyro))
 	}//else	if(ugol_povorota<0)
 	set_ugol_rul(0);
