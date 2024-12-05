6bfd39c59b1b7f05d85a9223d63746a7135c0aeb
AdreyP <andrewprokopchyk34@gmail.com>
2024-12-05 19:44:19 +0300

    поменял + и -

--- dec2024/lib/lib_mvGyroleftToRightBui.c
+++ dec2024/lib/lib_mvGyroleftToRightBui.c
@@ -14,15 +14,15 @@ void mvGyroLeftToRightBui(int giroTagetXZ, int LeftBui)
 	{
 			sleep(15);
 			int delta_distans_right =  distans_ot_robota_do_borta - SensorValue(port_left);
-			GiroscopTargetDinamik = GiroscopTargetFrozen - delta_distans_right;
+			GiroscopTargetDinamik = GiroscopTargetFrozen + delta_distans_right;
 			if(abs(delta_distans_right)<20)
 			{	correct_kurs( GiroscopTargetDinamik, 1 , v_max);	}
 			else
 			{
 				if(GiroscopTargetFrozen-GiroscopTargetDinamik>0)
-				{	correct_kurs( GiroscopTargetFrozen+20, 1 , v_max);	}
-				else
 				{	correct_kurs( GiroscopTargetFrozen-20, 1 , v_max);	}
+				else
+				{	correct_kurs( GiroscopTargetFrozen+20, 1 , v_max);	}
 			}//if(abs(delta_distans_right)<10)
 		sleep(15);
 	}// while( LeftBui<SensorValue(port_left) )
