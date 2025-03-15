ce54709bd09dd65d98ef80db3eb88d0f9403efcc
AndyDDT <usp@mail.ru>
2025-03-15 16:05:54 +0300

    dlog.py  конвертаци

--- apr2025/dlog.py
+++ apr2025/dlog.py
@@ -1,4 +1,26 @@
 #! python3
 
-fr = open('D:\shtil_robotc_ev3\\13ir.csv','r')
+#fr = open('D:\shtil_robotc_ev3\\13ir.csv','r')
 
+
+import csv
+ 
+with open('D:\shtil_robotc_ev3\\13ir.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        print(row)
+
+
+
+ 
+	
+import csv
+ 
+data = [['Name', 'Age', 'City'],
+        ['Alice', '30', 'New York'],
+        ['Bob', '25', 'San Francisco']]
+ 
+with open('output.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    for row in data:
+        csvwriter.writerow(row)   
